9824bdb7cb86a01058945d62e2fbb14670ed45c4
ningninw <ningninw@amazon.com>
2017-04-06 16:07:31 -0700

    add similarity compare algth

---
+++ cosineSimilarity.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+from math import*
+txt_file_A = open("hackday/topTracksRankingAmazon.txt", "r")
+txt_file_B = open("hackday/topTracksRankingYouTube.txt", "r")
+vector_A = txt_file_A.read().split()
+vector_B = txt_file_B.read().split()
+
+vector_A = list(map(int, vector_A))
+vector_B = list(map(int, vector_B))
+print vector_B
+ 
+def square_rooted(x):
+ 
+    return round(sqrt(sum([a*a for a in x])),3)
+ 
+def cosine_similarity(x,y):
+ 
+   numerator = sum(a*b for a,b in zip(x,y))
+   denominator = square_rooted(x)*square_rooted(y)
+   return round(numerator/float(denominator),3)
+ 
+print cosine_similarity(vector_A, vector_B)
