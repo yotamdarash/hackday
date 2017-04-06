ffed4061875d79af692612f35146a1b5450df114
daniel <danyotam@amazon.com>
2017-04-06 16:05:21 -0700

    add files

---
+++ similarityTask.py
@@ -0,0 +1,33 @@
+def addVector(trackVectorsCollection, chart, type):
+    for i,line in enumerate(chart):
+        title = line.split("\t")[0].strip();
+        if not trackVectorsCollection.has_key(title):
+            trackVectorsCollection[title] = {type : i}
+        else:
+            trackVectorsCollection[title][type] = i
+
+
+
+amazonFileName = "top100trackAmazonTitle.txt"
+youtubeFileName = "top100videoSortedListByReviews.txt"
+amazonRank = open(amazonFileName)
+youtubeRank = open(youtubeFileName)
+
+trackVectorsCollection = {}
+addVector(trackVectorsCollection,amazonRank,"amazon")
+addVector(trackVectorsCollection,youtubeRank,"youtube")
+
+
+youtubePlaceList = []
+amazonPlaceList = []
+listToFile = []
+for item in trackVectorsCollection.values():
+    if item.has_key("amazon") and item.has_key("youtube"):
+        youtubePlaceList.append(item["amazon"])
+        amazonPlaceList.append(item["youtube"])
+        listToFile.append(str(item["amazon"]) + " " + str(item["youtube"]))
+
+outfile = open("TracksTwoSourceVectors.txt", 'w')
+outfile.write("\n".join(listToFile))
+print trackVectorsCollection
+
