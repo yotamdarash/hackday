8494956da4ca8ece5bfcc199276d62899f9aed94
Zeng <yuchengz@amazon.com>
2017-04-06 22:06:56 -0700

    added Italy and India popularity data from Youtube

---
+++ sortYoutubePopularTitleByReviewsItaly.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+
+def score(line):
+    return long(line.split("\t")[1])
+
+if __name__ == "__main__":
+    orderedList = []
+    kind = "video"
+    if kind == "playlist":
+        file = open("topTracksFromYouTubeItaly.txt")
+    else:
+        file = open("topTracksFromYouTubeItaly.txt")
+
+    for i,line in enumerate(sorted(file, key=score, reverse=True)):
+        print(line)
+        orderedList.append(str(i) + " " + line)
+
+    outfile = open("top100YouTube" + kind + "SortedListByReviewsItaly.txt", 'w')
+    outfile.write("".join(orderedList))
